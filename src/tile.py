f6cdf8da5f6f355f0a91711dca925a597c9c9108
yousser <yousser@outlook.com>
2015-04-01 14:01:36 +0000

    Update tile.py
    Update commits

--- src/tile.py
+++ src/tile.py
@@ -1,41 +1,42 @@
 
 class Tile:
     """Generic Tile component;"""
+    # background color
+    BGCOLOR = "black"
+    # font style
+    FONT = "sans 35"
+    # dims 
+    WIDTH = 100     # pixels
+    # coords
+    X = 0
+    Y = 0
 
     def __init__(self, master, **kw):
     
         # public inits
-        self.bgcolor = kw.get("bgcolor", "black")
+        self.bgcolor = kw.get("bgcolor", self.BGCOLOR)
         self.ftcolor = "white" if self.bgcolor=="black" else "black"
-        self.font = kw.get("font", "sans 35")
-        self.width = kw.get("width", 100)
-        self.x = kw.get("x", 0)
-        self.y = kw.get("y", 0)
+        self.font = kw.get("font", self.FONT)
+        self.width = kw.get("width", self.WIDTH)
+        self.x = kw.get("x", self.X)
+        self.y = kw.get("y", self.Y)
 
         # private members
         self.__tk_owner = master
         self.__txt_tile = kw.get("text", 0)
 
         # widget inits
-        self.init_widget(**kw)
+        self.init_widget()
 
-    def init_widget(self, **kw):
+    def init_widget(self):
         "init's main widget."
         
-        # coords
-        _x, _y = self.x, self.y
-
-        # width and height tile
-        _wid = self.width
-
-        # background color
-        _bg = self.bgcolor
-
-        # font color
-        _fc = self.ftcolor
-
-        # font style
-        _fs = self.font
+        # shortcuts
+        _x, _y = self.x, self.y       # coords
+        _wid = self.width             # width and height tile  
+        _bg = self.bgcolor            # background color
+        _fc = self.ftcolor            # font color
+        _fs = self.font               # font style
 
         # create rectangle (exactly square)
         self.rectangle = self.__tk_owner.create_rectangle(
@@ -45,7 +46,7 @@ class Tile:
         # create text in centre of rectangle
         self.__tk_owner.create_text (
             _x + _wid/2, _y + _wid/2, text = self.txt_tile, font=_fs, fill=_fc
-            )
+        )
 
     @property
     def txt_tile(self):
